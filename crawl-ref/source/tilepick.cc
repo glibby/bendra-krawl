155c654fd879ae749c8fa28536c0c781b5538422
Richard McCormack <brick@brick.codes>
2024-08-02 13:12:40 -0400

    More AK fixes

--- crawl-ref/source/tilepick.cc
+++ crawl-ref/source/tilepick.cc
@@ -4341,6 +4341,8 @@ static tileidx_t _tileidx_player_job_base(const job_type job)
             return TILEG_JOB_REAVER;
         case JOB_CINDER_ACOLYTE:
             return TILEG_JOB_CINDER_ACOLYTE;
+        case JOB_ABYSSAL_KNIGHT:
+            return TILEG_JOB_ABYSSAL_KNIGHT;
         default:
             return TILEG_ERROR;
     }
